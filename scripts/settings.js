f1ad490d703c6c2d40dd9ef04a765a6659486c34
Bruce McPherson <bruce@mcpher.com>
2015-02-14 09:43:06 +0000

    updated by GasGit automation

---
+++ scripts/settings.js
@@ -0,0 +1,28 @@
+"use strict";
+/**
+ * SET THIS STUFF TO WHATEVER YOU NEED
+ */
+var SETTINGS= {
+  EXTRACT: { // where to put the sources to
+    TO:"/Extraction/Scripts"
+  },
+  PARENT: {  // where to start looking for scripts
+    SCRIPTS:[
+      "/Published Scripts"
+    ]
+  },
+  DEPENDENCY: { // things for dependency management
+    LIST: {
+      URL: "https://script.google.com/macros/s/AKfycbwZ2Hht93wTNzvRmYINYF7obaOHciBXWcP_wAiEtyGq70_x3cI/exec"
+    }
+  },
+  GIT: {
+    COMMITTER: {
+      "name": "Bruce McPherson",
+      "email": "bruce@mcpher.com"
+    },
+    USERAGENT: "brucemcpherson",
+    LIBRARIES: "libraries/",
+    SCRIPTS: "scripts/"
+  }
+};
