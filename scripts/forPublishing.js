620bfe49d0c3f71b8c2f3a15674c8d8af1849b9b
Bruce McPherson <bruce@mcpher.com>
2015-04-30 12:48:33 +0100

    updated by GasGit automation

--- scripts/forPublishing.js
+++ scripts/forPublishing.js
@@ -7,7 +7,7 @@ function getLibraryInfo () {
   return { 
     info: {
       name:'gasGit',
-      version:'0.0.1',
+      version:'0.0.2',
       key:'M6Heerx1czXDLw7NL4S7pdKi_d-phDA33',
       description:'extract sources from apps script and write them to github',
       share:'https://script.google.com/d/1TphrUjRcx5sGlhgkfjB2R9MOZe3cPF7wK1LV8yVNoFCAwRTeNyXVsDFd/edit?usp=sharing'
