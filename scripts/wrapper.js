3254b03a81d99788733988f4ae92bca255eba489
Bruce McPherson <bruce@mcpher.com>
2015-09-15 12:03:07 +0100

    updated by GasGit automation

---
+++ scripts/wrapper.js
@@ -0,0 +1,12 @@
+function wrapper () {
+  //sp
+
+  ['addons','advancedservices','contentservice','externalapis','gmail','htmlservice','spreadsheetapp','vbalibrary'].forEach(function(d) {
+    SETTINGS.EXTRACT.TO = "/books/going gas/assets/goinggas/"+ d +"/gas/source";
+    SETTINGS.PARENT.SCRIPTS = ["/books/going gas/assets/goinggas/"+ d +"/gas/scripts"];
+    doExtraction();
+    //library service not working for now
+    //doLibraries();
+  });
+
+}
