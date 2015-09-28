08affe02a3e521e324a222c4726c44d88cd8f80d
Bruce McPherson <bruce@mcpher.com>
2015-09-28 18:02:12 +0100

    updated by GasGit automation

--- scripts/wrapper.js
+++ scripts/wrapper.js
@@ -1,13 +1,21 @@
 function wrapper () {
   //sp
 
-  ['addons','advancedservices','contentservice','externalapis','gmail','htmlservice','spreadsheetapp','vbalibrary'].forEach(function(d) {
-    
+  ['executionapi','addons','advancedservices','contentservice','externalapis','gmail','htmlservice','spreadsheetapp','vbalibrary'].forEach(function(d) {
+   
     SETTINGS.EXTRACT.TO = "/books/going gas/assets/goinggas/"+ d +"/gas/source";
     SETTINGS.PARENT.SCRIPTS = ["/books/going gas/assets/goinggas/"+ d +"/gas/scripts"];
     doExtraction();
-    doLibraries();
+    doLibraries(); 
     
   });
 
+    ['executionapi'].forEach(function(d) {
+   
+    SETTINGS.EXTRACT.TO = "/Extraction/repos/"+ d +"/gas/source";
+    SETTINGS.PARENT.SCRIPTS = ["/books/going gas/assets/goinggas/"+ d +"/gas/scripts"];
+    doExtraction();
+    doLibraries(); 
+    
+  });
 }
