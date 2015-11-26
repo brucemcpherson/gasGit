6f6800d568287d7961cee3e759eae1ea3ab4c34c
Bruce McPherson <bruce@mcpher.com>
2015-11-26 20:01:03 +0000

    updated by GasGit automation

--- libraries/cGoa/Goa.js
+++ libraries/cGoa/Goa.js
@@ -1,6 +1,11 @@
 /**
- * an oauth2 servicing class
- */
+* create a goa class
+* @constructor
+* @param {string} packageName the package name
+* @param {PropertyStore} propertyStore the property store
+* @param {number} [optTimeout] in seconds
+* @param {string} [impersonate] email address to impersonate for service accounts
+*/
 var Goa = function (packageName, propertyStore, optTimeout , impersonate) {
   'use strict';
 
@@ -19,6 +24,7 @@ var Goa = function (packageName, propertyStore, optTimeout , impersonate) {
       name_,
       onToken_,
       onTokenResult_;
+     
 
 
   /**
@@ -83,19 +89,20 @@ var Goa = function (packageName, propertyStore, optTimeout , impersonate) {
     // need to store these for later
       id_ = cUseful.generateUniqueString();
       GoaApp.cachePut ( id_ , package_.packageName , params_, onToken_);
+      var offline = cUseful.applyDefault(package_.offline, true);
       
       needsConsent_ = consentScreen_ || GoaApp.defaultConsentScreen ( GoaApp.createAuthenticationUri ( 
         package_, {
           callback : callback_,
           timeout: timeout_,
-          offline:true,
+          offline:offline,
           force: true
         }, {
           goaid:id_,
           goaphase:'fetch',
           goaname:package_.packageName
-        }) ,GoaApp.createRedirectUri(), package_.packageName, package_.service);
-      
+        }) ,GoaApp.createRedirectUri(), package_.packageName, package_.service, offline);
+
       return self;
     }
     
@@ -233,6 +240,21 @@ var Goa = function (packageName, propertyStore, optTimeout , impersonate) {
     package_.revised = new Date().getTime();
     GoaApp.setPackage ( propertyStore_ , package_);
     return self;
+  };
+  
+  /**
+   * kill the package
+   */
+  self.kill = function () {
+    GoaApp.kill(package_);
+    return self.writePackage();
+  };
+  
+  /**
+   * remove the package
+   */
+  self.remove = function () {
+    return GoaApp.removePackage ( propertyStore_, package_.packageName  );
   };
   
   
