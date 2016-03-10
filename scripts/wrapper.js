30441a591208816e17243bf7d14209ec98ee8b1b
Bruce McPherson <bruce@mcpher.com>
2016-03-10 14:16:08 +0000

    updated by GasGit automation

--- scripts/wrapper.js
+++ scripts/wrapper.js
@@ -3,6 +3,7 @@
  * you can do this to create a folder from 
  * various scripts
  * then use the regulr git client to push
+ * here's some examples of how to fiddle around with this
  */
 function wrapper () {
   //sp
@@ -24,4 +25,49 @@ function wrapper () {
     doLibraries(); 
     
   });
+}
+/**
+ * if you want to use the regular git client,
+ * you can do this to create a folder from 
+ * various scripts
+ * then use the regulr git client to push
+ */
+function videosWrapper () {
+  
+  // get a handler
+  var dapi = new cDriveJsonApi.DriveJsonApi()
+  .setAccessToken(getAccessToken('script'));
+  var c = [1,2,3,4,5,6,7,8,9,10,11,12,13,16,15,16];
+  
+  var chapters = c.map(function(d,i) {
+    return '/books/going gas/admin/video/assets/Chapter ' + (i+1) ;
+  });
+  
+  chapters.push('/books/going gas/admin/video/assets/Shared');
+  
+  // get all the folders
+  var folders = chapters.map(function(d) {
+
+    return {
+      scripts:dapi.getFolderFromPath(d,false),
+      containers:dapi.getFolderFromPath(d,false),
+      source:dapi.getFolderFromPath(d + '/source',false),
+      path:d
+    }
+    
+  });
+  
+  // get any script files
+
+  folders.forEach(function(d) {
+    if (d.source) {
+      SETTINGS.EXTRACT.TO = d.path + '/source';
+      SETTINGS.PARENT.SCRIPTS = [d.path];
+      
+      doExtraction();
+      doLibraries(); 
+    }
+  });
+  
+
 }
